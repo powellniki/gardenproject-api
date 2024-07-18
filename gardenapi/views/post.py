a58f973810b36f19104be3d3be6c9f165014d048
Niki Powell <nhe.powell@gmail.com>
2024-07-18 14:33:29 -0500

    created post view

---
+++ gardenapi/views/post.py
@@ -0,0 +1,29 @@
+from rest_framework.decorators import action
+from django.core.files.base import ContentFile
+from django.http import HttpResponseServerError
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import serializers
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
+
+class Posts(ViewSet):
+    """Request handlers for Posts in the Garden Platform"""
+
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+
+    def create(self, request):
+        pass
+
+    def retrieve(self, request, pk=None):
+        pass
+
+    def update(self, request, pk=None):
+        pass
+
+    def destroy(self, request, pk=None):
+        pass
+
+    def list(self, request):
+        pass
