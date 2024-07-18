3542e7a53865db8ec62d48c6df0bf8bce46cb05d
Niki Powell <nhe.powell@gmail.com>
2024-07-18 13:18:50 -0500

    topics fixture

--- seed_database.sh
+++ seed_database.sh
@@ -8,8 +8,8 @@ python3 manage.py migrate gardenapi
 python3 manage.py loaddata users
 python3 manage.py loaddata tokens
 python3 manage.py loaddata gardeners
-# python3 manage.py loaddata comments
 python3 manage.py loaddata posts
+python3 manage.py loaddata topics
 # python3 manage.py loaddata posttopics
-# python3 manage.py loaddata topics
+# python3 manage.py loaddata comments
 
