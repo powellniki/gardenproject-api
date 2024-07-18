67b520be13f3478885e77492876789219fcee24e
Niki Powell <nhe.powell@gmail.com>
2024-07-18 15:34:47 -0500

    serialized gardener for posts

--- gardenapi/views/post.py
+++ gardenapi/views/post.py
@@ -1,4 +1,5 @@
 from rest_framework.decorators import action
+from django.contrib.auth.models import User
 from django.core.files.base import ContentFile
 from django.http import HttpResponseServerError
 from rest_framework.viewsets import ViewSet
@@ -6,7 +7,7 @@ from rest_framework.response import Response
 from rest_framework import serializers
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
-from gardenapi.models import Post
+from gardenapi.models import Post, Gardener
 
 
 
@@ -30,15 +31,30 @@ class Posts(ViewSet):
 
     def list(self, request):
         posts = Post.objects.all()
-        serialized = PostSerializer(posts, many=True, context={'request': request})
+        serialized = PostSerializer(posts, many=True,)
         return Response(serialized.data, status=status.HTTP_200_OK)
     
 
+class PostOwnerSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ('username',)
+
+
+class GardenerSerializer(serializers.ModelSerializer):
+    user = PostOwnerSerializer(many=False)
+
+    class Meta:
+        model = Gardener
+        fields = ('user',)
+
+
 class PostSerializer(serializers.ModelSerializer):
-    """JSON serializer for Posts"""
+    gardener = GardenerSerializer(many=False)
 
     class Meta:
         model = Post
         url = serializers.HyperlinkedIdentityField(view_name="post", lookup_field="id")
-        fields = ('created_date', 'title', 'description', 'gardener')
+        fields = ('created_date', 'title', 'description', 'gardener',)
         depth = 1
