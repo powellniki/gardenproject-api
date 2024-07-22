475c68291c3e2a48b51bacd00a8917c01c10b7d3
Niki Powell <nhe.powell@gmail.com>
2024-07-22 16:53:53 -0400

    added retrieve to post

---
+++ gardenapi/views/comment.py
@@ -0,0 +1,11 @@
+from rest_framework.viewsets import ViewSet
+from rest_framework import serializers
+from rest_framework.response import Response
+from rest_framework import status
+from gardenapi.models import Comment
+
+
+class Comments(ViewSet):
+    pass
+
+
--- gardenapi/views/post.py
+++ gardenapi/views/post.py
@@ -2,13 +2,14 @@ from rest_framework.decorators import action
 from django.contrib.auth.models import User
 from django.core.files.base import ContentFile
 from django.http import HttpResponseServerError
+from django.utils import timezone
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from django.db import models
-from gardenapi.models import Post, Gardener, Topic, Comment
+from gardenapi.models import Post, Gardener, PostTopic, Topic, Comment
 
 
 
@@ -19,10 +20,57 @@ class Posts(ViewSet):
     permission_classes = (IsAuthenticatedOrReadOnly,)
 
     def create(self, request):
-        pass
 
+        # make sure gardener is authenticated
+        if not request.user.is_authenticated:
+            return Response({'error': 'Please provide authentication credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+        
+        # Initialize the create serializer with request data and context
+        serializer = PostCreateSerializer(data=request.data, context={'request': request})
+        
+        # Validate the serializer
+        if serializer.is_valid():
+            validated_data = serializer.validated_data
+            title = validated_data.get('title')
+            description = validated_data.get('description')
+            posttopics_ids = validated_data.get('posttopics', [])
+
+            # Get the gardener associated with the authenticated user
+            try:
+                gardener = Gardener.objects.get(user=request.user)
+            except Gardener.DoesNotExist:
+                return Response({'error': 'Gardener does not exist'}, status=status.HTTP_404_NOT_FOUND)
+
+            # Create and save the new post instance
+            new_post = Post.objects.create(
+                title=title,
+                description=description,
+                gardener=gardener,
+                created_date=timezone.now().date()
+            )
+
+            # Establish many-to-many relationships with post-topics
+            for topic_id in posttopics_ids:
+                try:
+                    topic = Topic.objects.get(id=topic_id)
+                    PostTopic.objects.create(post=new_post, topic=topic)
+                except Topic.DoesNotExist:
+                    continue
+
+            # Serialize and return the new post instance
+            response_serializer = PostSerializer(new_post, context={'request': request})
+            return Response(response_serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            # Return validation errors if any
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
     def retrieve(self, request, pk=None):
-        pass
+        try:
+            post = Post.objects.get(pk=pk)
+            serializer = PostSerializer(post, context={"request": request})
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Post.DoesNotExist:
+            return Response({'error': 'Post not found'}, status=status.HTTP_404_NOT_FOUND)
 
     def update(self, request, pk=None):
         pass
@@ -40,15 +88,17 @@ class Posts(ViewSet):
         else:
             posts = Post.objects.all()
 
-        serialized = PostSerializer(posts, many=True)
-        return Response(serialized.data, status=status.HTTP_200_OK)
-    
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
 
 
 class TopicSerializer(serializers.ModelSerializer):
     class Meta:
         model = Topic
         fields = ('name',)
+
 
 class GardenerSerializer(serializers.ModelSerializer):
     username = serializers.ReadOnlyField()
@@ -57,15 +107,32 @@ class GardenerSerializer(serializers.ModelSerializer):
         model = Gardener
         fields = ('username',)
 
+
+class PostCreateSerializer(serializers.ModelSerializer):
+    posttopics = serializers.ListField(child=serializers.IntegerField(), write_only=True, required=False)
+
+    class Meta:
+        model = Post
+        fields = ('title', 'description', 'posttopics',)
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    gardener = GardenerSerializer(many=False)
+
+    class Meta:
+        model = Comment
+        fields = ('comment', 'date', 'gardener',)
+
+
 class PostSerializer(serializers.ModelSerializer):
     gardener = GardenerSerializer(many=False)
     comment_count = serializers.SerializerMethodField()
     topics = TopicSerializer(many=True)
+    comments = CommentSerializer(many=True, read_only=True)
 
     class Meta:
         model = Post
-        url = serializers.HyperlinkedIdentityField(view_name="post", lookup_field="id")
-        fields = ('created_date', 'title', 'description', 'gardener', 'comment_count', 'topics',)
+        fields = ('created_date', 'title', 'description', 'gardener', 'comment_count', 'comments', 'topics',)
         depth = 1
 
     def get_comment_count(self, obj):
