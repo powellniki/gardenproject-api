139277ccc099fcc92ed4194f8f7888aec55042d8
Niki Powell <nhe.powell@gmail.com>
2024-08-01 14:35:46 -0500

    cors allowed methods

--- gardenproject/settings.py
+++ gardenproject/settings.py
@@ -71,6 +71,15 @@ CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:5173'
 ]
 
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
+
 ROOT_URLCONF = 'gardenproject.urls'
 
 TEMPLATES = [
